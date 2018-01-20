2809c11bef33725a184512caef8323f711c4a718
patrickf2000 <patrick_dev2000@outlook.com>
2018-01-20 10:16:50 -0500

    updated build-winbin script

--- build-winbin.bat
+++ build-winbin.bat
@@ -29,14 +29,34 @@
 ::Builds a winbin folder
 ::This folder contains all the files needed to build a distributable CppEditor
 ::You must do this before building the setup.iss script.
+cls
+echo You must enter the path to where the Qt DLLs are.
+echo For example, if you built CppEditor using Qt 5.10.0 with the minw53_32 toolkit,
+echo your path will look like this: C:\Qt\5.10.0\mingw53_32
+echo.
+echo Warning:
+echo DO NOT include "bin" in the name, even though the DLLs are in that folder.
+echo DO NOT include the ending slash.
+echo.
+set /p qtpath="Enter the complete path to the folder: "
+echo %qtpath%
 
+cls
+if not exist %qtpath% (
+	echo Error
+	echo Unknown path.
+	echo Exiting...
+	exit /B
+)
+
+cls
 echo Checking for folders...
 if exist ".\winbin" rmdir .\winbin
 md winbin
 
 echo Copying executable...
-if exist ".\build\src\debug\CppEditor.exe" (
-	copy .\build\src\debug\CppEditor.exe .\winbin\CppEditor.exe
+if exist ".\build\src\CppEditor.exe" (
+	copy .\build\src\CppEditor.exe .\winbin\CppEditor.exe
 ) else (
 	echo Program executable not found.
 	echo Please make sure the program is built.
@@ -48,16 +68,20 @@ cd winbin
 
 md platforms
 cd platforms
-copy "C:\Qt\5.10.0\mingw53_32\plugins\platforms\qwindows.dll" .\
+copy "%qtpath%\plugins\platforms\qwindows.dll" .\
 cd ..
 
-copy "C:\Qt\5.10.0\mingw53_32\bin\libEGL.dll" .\
-copy "C:\Qt\5.10.0\mingw53_32\bin\libgcc_s_dw2-1.dll" .\
-copy "C:\Qt\5.10.0\mingw53_32\bin\libstdc++-6.dll" .\
-copy "C:\Qt\5.10.0\mingw53_32\bin\libwinpthread-1.dll" .\
-copy "C:\Qt\5.10.0\mingw53_32\bin\Qt5Cored.dll" .\
-copy "C:\Qt\5.10.0\mingw53_32\bin\Qt5Guid.dll" .\
-copy "C:\Qt\5.10.0\mingw53_32\bin\Qt5Widgetsd.dll" .\
+copy "%qtpath%\bin\libEGL.dll" .\
+copy "%qtpath%\bin\libgcc_s_dw2-1.dll" .\
+copy "%qtpath%\bin\libstdc++-6.dll" .\
+copy "%qtpath%\bin\libwinpthread-1.dll" .\
+copy "%qtpath%\bin\Qt5Core.dll" .\
+copy "%qtpath%\bin\Qt5Gui.dll" .\
+copy "%qtpath%\bin\Qt5Widgets.dll" .\
+
+copy "C:\Unix\bin\libKF5DBusAddons.dll" .\
+copy "C:\Unix\bin\libKF5SyntaxHighlighting.dll" .\
+copy "C:\Unix\bin\libsettings.dll" .\
 
 cd ..
 
